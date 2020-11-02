fb7a2acfbe9988725e79ee331e533909e6f90f6f
Gabriel Cardona <gabriel@avalabs.org>
2020-11-02 07:17:50 -0800

    Porting over key components.

---
+++ src/apis/contractvm/credentials.ts
@@ -0,0 +1,58 @@
+/**
+ * @packageDocumentation
+ * @module API-ContractVM-Credentials
+ */
+import BinTools from '../../utils/bintools';
+
+import { ContractVMConstants } from './constants';
+import { Credential } from '../../common/credentials';
+import { Serialization } from '../../utils/serialization';
+
+/**
+ * @ignore
+ */
+const bintools:BinTools = BinTools.getInstance();
+const serializer = Serialization.getInstance();
+
+/**
+ * Takes a buffer representing the credential and returns the proper [[Credential]] instance.
+ *
+ * @param credid A number representing the credential ID parsed prior to the bytes passed in
+ *
+ * @returns An instance of an [[Credential]]-extended class.
+ */
+export const SelectCredentialClass = (credid:number, ...args:Array<any>):Credential => {
+  if (credid === ContractVMConstants.SECPCREDENTIAL) {
+    return new SECPCredential(...args);
+  }
+  /* istanbul ignore next */
+  throw new Error(`Error - SelectCredentialClass: unknown credid ${credid}`);
+};
+
+export class SECPCredential extends Credential {
+  protected _typeName = "SECPCredential";
+  protected _typeID = ContractVMConstants.SECPCREDENTIAL;
+
+  //serialize and deserialize both are inherited
+
+  getCredentialID():number {
+    return this._typeID;
+  }
+
+
+  clone():this {
+    let newbase:SECPCredential = new SECPCredential();
+    newbase.fromBuffer(this.toBuffer());
+    return newbase as this;
+  }
+
+  create(...args:any[]):this {
+    return new SECPCredential(...args) as this;
+  }
+
+  select(id:number, ...args:any[]):Credential {
+    let newbasetx:Credential = SelectCredentialClass(id, ...args);
+    return newbasetx;
+  }
+}
+
